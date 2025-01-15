bbc80b4a4f09895951ed488bbdbd8296ce7e77f1
srinivasas-zluri <srinivasa.s@zluri.com>
2025-01-15 17:42:51 +0530

    implemented tests for application

--- backend/tests/app/app.test.ts
+++ backend/tests/app/app.test.ts
@@ -3,8 +3,8 @@ import request from "supertest";
 import app, { startApp } from "~/app";
 import testDBConfig from "../mikro-orm.test-config";
 import { DBServices } from "~/db";
-import { TransactionService } from "~/services/transaction.service";
 import { Transaction } from "~/models/transaction";
+import { FileCSVWriter } from "~/internal/csv/writer";
 
 describe("API Routes testing", () => {
   let server: http.Server;
@@ -33,6 +33,10 @@ describe("API Routes testing", () => {
   beforeEach(async () => {
     // reset mocks
     jest.resetAllMocks();
+
+    // reset the db
+    const db: DBServices = await app.get("db");
+    await db.em.nativeDelete(Transaction, {});
   });
 
   it("should upload a file and create transactions", async () => {
@@ -51,5 +55,125 @@ describe("API Routes testing", () => {
     );
     // You can also add more checks to ensure the transactions have been saved in the DB
   });
- 
+
+  // write duplicates
+  it("should upload a file and create transactions with duplicates", async () => {
+    const file = Buffer.from(`date,amount,description,currency
+      08-01-2024,100,payment,cad
+      09-01-2024,200,purchase,usd
+      `);
+    const response = await request(app)
+      .post("/api/v1/transaction/upload")
+      .attach("file", file, "test-file.csv");
+
+    expect(response.status).toBe(201);
+
+    const file2 = Buffer.from(`date,amount,description,currency
+      07-01-2024,100,paymeNt,cad
+      08-01-2024,100,paymeNt,cad
+      09-01-2024,200,puRchase,usd
+      09-01-2024,200,puRchasE,usd
+      `);
+
+    const response2 = await request(app)
+      .post("/api/v1/transaction/upload")
+      .attach("file", file2, "test-file.csv");
+
+    expect(response2.status).toBe(200);
+    // read the file sent back
+  });
+
+  // mock the write rows fn to return an error
+  it("should return 500 if there is an error writing to the file", async () => {
+    const file = Buffer.from(`date,amount,description,currency
+      08-01-2024,100,payment,cad
+      09-01-2024,200,purchase,usd
+      `);
+    const response = await request(app)
+      .post("/api/v1/transaction/upload")
+      .attach("file", file, "test-file.csv");
+
+    expect(response.status).toBe(201);
+
+    const file2 = Buffer.from(`date,amount,description,currency
+      07-01-2024,100,paymeNt,cad
+      08-01-2024,100,paymeNt,cad
+      09-01-2024,200,puRchase,usd
+      09-01-2024,200,puRchasE,usd
+      `);
+
+    jest
+      .spyOn(FileCSVWriter.prototype, "writeRows")
+      .mockImplementation(async (x) => {
+        throw new Error("Error writing to CSV file");
+      });
+
+    const response2 = await request(app)
+      .post("/api/v1/transaction/upload")
+      .attach("file", file2, "test-file.csv");
+
+    expect(response2.status).toBe(500);
+    expect(response2.body.message).toBe("Failed to create transactions");
+  });
+
+  it("should return 400 if no file is uploaded", async () => {
+    const response = await request(app)
+      .post("/api/v1/transaction/upload")
+      .send({});
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("No file uploaded");
+  });
+
+  it("should return 400 if file type is not csv", async () => {
+    const file = Buffer.from(`date,amount,description,currency
+      08-01-2024,100,payment,cad
+      09-01-2024,200,purchase,usd
+      `);
+    const response = await request(app)
+      .post("/api/v1/transaction/upload")
+      .attach("file", file, "test-file.txt");
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("Invalid file type");
+  });
+
+  it("should return 500 if there is an error writing to the file", async () => {
+    const file = Buffer.from(`date,amount,description,currency
+      08-01-2024,100,payment,cad
+      09-01-2024,200,purchase,usd
+      `);
+    const response = await request(app)
+      .post("/api/v1/transaction/upload")
+      .attach("file", file, "test-file.csv");
+
+    expect(response.status).toBe(201);
+
+    const file2 = Buffer.from(`date,amount,description,currency
+      07-01-2024,100,paymeNt,cad
+      08-01-2024,100,paymeNt,cad
+      09-01-2024,200,puRchase,usd
+      09-01-2024,200,puRchasE,usd
+      `);
+
+    let callCount = 0;
+    jest
+      .spyOn(FileCSVWriter.prototype, "writeRows")
+      .mockImplementation(async (x) => {
+        if (callCount == 0) {
+          callCount++;
+          return null;
+        }
+        return { type: "WriteError", message: "Error writing to CSV file" };
+      });
+
+    const response2 = await request(app)
+      .post("/api/v1/transaction/upload")
+      .attach("file", file2, "test-file.csv");
+
+    expect(response2.status).toBe(500);
+    expect(response2.body.message).toBe(
+      "Server failed to generate an error file"
+    );
+  });
 });
