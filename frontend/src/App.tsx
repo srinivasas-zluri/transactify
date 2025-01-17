75cd6846320b5f44620b743f78469c865fd205c2
srinivasas-zluri <srinivasa.s@zluri.com>
2025-01-18 00:29:52 +0530

    🖌️UI:  ui changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-import { TbTrashXFilled } from "react-icons/tb";
+import { TbFileSpreadsheet, TbTrashXFilled } from "react-icons/tb";
+import { TbCloudUpload } from "react-icons/tb";
 import { TbEdit } from "react-icons/tb";
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
-import { Table, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { FaFileUpload } from 'react-icons/fa';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import 'react-toastify/dist/ReactToastify.css';
 import 'tailwindcss/tailwind.css';
 
@@ -13,20 +13,20 @@ import { useFileUpload } from './hooks/useFileUpload';
 import { useTransactions } from './hooks/useTransaction';
 import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
 
-// create an enum with the table state 
 enum TableState {
   Loading,
+  UploadingFile,
   View,
   Edit,
   Error,
 }
 
 const App = () => {
-  const { file, setFile, loading, progress, handleFileUpload } = useFileUpload();
-  const [page, setPage] = useState<number>(1);
+  const { progress, handleFileUpload } = useFileUpload();
+  const [page, setPage] = useState<number>(9);
   const { transactions, handleDelete, prevNext, handleUpdate, fetchTransactions } = useTransactions();
   const { prevPage: prev, nextPage: next } = prevNext;
-  const [tableState, setTableState] = useState<TableState>(TableState.Loading);
+  const [pageState, setPageState] = useState<TableState>(TableState.Loading);
 
 
 
@@ -34,9 +34,9 @@ const App = () => {
     // TODO: debouncing
     // TODO: don't disable the lint use `useCallback` to memoize the function
     const loadTransactions = async () => {
-      setTableState(TableState.Loading);
+      setPageState(TableState.Loading);
       await fetchTransactions(page);  // Use the function directly
-      setTableState(TableState.View);
+      setPageState(TableState.View);
     };
 
     loadTransactions();
@@ -47,7 +47,12 @@ const App = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page]);
 
-  if (tableState === TableState.Loading) {
+  if (pageState === TableState.UploadingFile) {
+    // return progress bar 
+    return <Progress value={progress} className="w-[60%]" />
+  }
+
+  if (pageState === TableState.Loading) {
     // TODO: Change the loadign state later
     return (
       <div className="bg-gray-50 shadow-lg mx-auto p-8 rounded-lg max-w-7xl">
@@ -60,25 +65,25 @@ const App = () => {
   }
 
   return (
-    <div className="bg-gray-50 shadow-lg mx-auto p-8 rounded-lg max-w-7xl">
+    <div className="mx-auto p-8 rounded-lg max-w-7xl">
       <ToastContainer />
       <h1 className="mb-8 font-semibold text-4xl text-center text-gray-800">Transaction Management</h1>
+      <UploadFile onUpload={async (file: File) => {
+        setPageState(TableState.UploadingFile);
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+        await handleFileUpload(file);
+        await fetchTransactions(page);
+        setPageState(TableState.View);
+      }} />
 
       {/* File Upload Section */}
       <div className="flex md:flex-row flex-col items-start md:items-center mb-6">
-        <input
+        {/* <input
           type="file"
           onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
           className="border-2 border-yellow-400/10 mb-2 md:mb-0 p-3 rounded-lg w-full md:w-80"
-        />
-        <Button
-          onClick={handleFileUpload}
-          disabled={loading || !file}
-          className="flex justify-center items-center space-x-2 border-2 border-green-400 bg-green-400/10 ml-0 md:ml-4 rounded-lg w-full md:w-auto text-green-800"
-        >
-          <FaFileUpload />
-          <span>Upload File</span>
-        </Button>
+        /> */}
+
       </div>
 
       {progress > 0 && progress < 100 && (
@@ -87,18 +92,18 @@ const App = () => {
 
       {/* Transaction Table */}
       <Table>
-        <TableHeader>
-          <TableRow className='bg-gray-200 text-left'>
-            <TableHead> Date </TableHead>
+        <TableHeader className='top-0 z-10 sticky mt-4'>
+          <TableRow className='bg-gray-200 hover:bg-gray-200 rounded-lg text-left'>
+            <TableHead className='p-5'> Date </TableHead>
             <TableHead> Description </TableHead>
             <TableHead> Amount </TableHead>
             <TableHead>Currency</TableHead>
             <TableHead>Actions</TableHead>
           </TableRow>
         </TableHeader>
-        <tbody>
+        <TableBody>
           {transactions.map((transaction) => (
-            <TableRow>
+            <TableRow key={transaction.id}>
               <TableCell className='px-4 py-2'> {transaction.transaction_date_string} </TableCell>
               <TableCell className='px-4 py-2'>
                 <ExpandableDescription description={transaction.description} />
@@ -109,7 +114,7 @@ const App = () => {
                 <div className='flex h-full'>
                   <Button
                     onClick={() => handleUpdate(transaction.id, 100)}
-                    className="flex items-center border-2 bg-transparent hover:bg-transparent shadow-none px-4 py-4 border-none h-full text-slate-300 hover:text-yellow-500"
+                    className="flex items-center border-2 bg-transparent hover:bg-transparent shadow-none px-4 py-3 border-none h-full text-slate-300 hover:text-yellow-500"
                   >
                     <TbEdit className='scale-150' />
                   </Button>
@@ -123,7 +128,7 @@ const App = () => {
               </TableCell>
             </TableRow>
           ))}
-        </tbody>
+        </TableBody>
       </Table>
 
       {/* Pagination */}
@@ -160,6 +165,60 @@ function ExpandableDescription({ description }: { description: string }) {
       )}
     </div>
   );
+}
+
+function UploadFile({ onUpload }: { onUpload: (file: File) => Promise<void> }) {
+
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const uploadedFile = e.target.files ? e.target.files[0] : null;
+    setFile(uploadedFile);
+    console.log({ file, uploadedFile });
+    if (uploadedFile == null || uploadedFile === undefined) {
+      toast.error('No file selected');
+      return;
+    }
+
+    try {
+      await onUpload(uploadedFile);
+      console.log("Got here")
+      setFile(null);
+    } catch (error) {
+      console.error(error);
+      toast.error('File upload failed');
+      setFile(null);
+    }
+  }
+
+  return <div className='w-full'>
+    <div className="mb-4">
+      <div className="relative flex justify-center items-center w-ful">
+        <label className="relative flex flex-col justify-center items-center border-2 border-slate-400 hover:border-gray-400 bg-slate-100/10 hover:bg-slate-100 border-dashed rounded-lg w-full max-w-2xl transition-all cursor-pointer">
+          {
+            (file == null) && <><TbCloudUpload className='p-5 text-slate-800 size-28' />
+              <span className="mt-2 px-5 pb-2 text-slate-600">
+                Click to upload a CSV file with transactions
+              </span>
+              <input
+                type="file"
+                accept='.csv'
+                onChange={async (e) => { await handleUpload(e); }}
+                className="absolute inset-0 opacity-0 cursor-pointer"
+              />
+            </>
+          }
+          {(file != null) && <>
+            <TbFileSpreadsheet className='p-5 text-slate-800 size-28' />
+            <span className="mt-2 px-5 pb-2 text-slate-600">
+              {file.name}
+            </span>
+          </>}
+
+        </label>
+      </div>
+    </div>
+  </div>
 }
 
 export default App;
