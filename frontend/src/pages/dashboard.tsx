6411589bb585872e787a9e852a82eed23e57a872
srinivasas-zluri <srinivasa.s@zluri.com>
2025-01-24 12:20:59 +0530

    refactor: enhance Dashboard component structure and improve analytics display

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -28,7 +28,7 @@ export const Dashboard = ({ back }: { back: () => void }) => {
     const [loading, setLoading] = useState(true);
     const [granularity, setGranularity] = useState<Granularity>('month');
     const [dateRange, setDateRange] = useState<DateRange>(undefined);
-    const [groupByCurrency, ] = useState(false);
+    const [groupByCurrency,] = useState(false);
 
     async function updateAnalyticsData() {
         try {
@@ -117,12 +117,11 @@ export const Dashboard = ({ back }: { back: () => void }) => {
 
     if (loading) return <TransactionManagementLoadingPage />;
 
-    if (!analyticsData) return <div>No data found</div>;
-
     const chartData = processChartData();
 
     return (
         <div className="space-y-8 p-8">
+            {/* Header of the whole page */}
             <header className="mb-6">
                 {/* back button */}
                 <button onClick={back} className="flex items-center space-x-2 mb-6 text-gray-600 hover:text-gray-800">
@@ -133,74 +132,114 @@ export const Dashboard = ({ back }: { back: () => void }) => {
                 <h1 className="font-semibold text-3xl text-gray-800">Transaction Analytics Dashboard</h1>
             </header>
 
-            {/* Granularity & Group By Options */}
-            <div>
-                {/* show a settings icon and big text */}
-                <div className="flex items-center mb-6">
-                    <TbSettings className="mr-2 text-2xl" />
-                    <h2 className="mb-2 font-semibold text-2xl text-gray-800">Settings </h2>
-                </div>
-                <div className="flex flex-wrap gap-4 mb-6">
-                    {/* <div className="flex items-center space-x-2">
+
+
+            {/* show the data if analytics is present */}
+            {(analyticsData && analyticsData.length > 0) ? (
+                <>
+                    <DashBoardSettings
+                        dateRange={dateRange}
+                        setGranularity={setGranularity}
+                        granularity={granularity}
+                        setDateRange={setDateRange}
+                        updateAnalyticsData={updateAnalyticsData}
+                        updateDaterange={updateDaterange}
+                    />
+                    <ChartDashboardQuickInsights analyticsData={analyticsData} granularity={granularity} />
+
+                    <div className="bg-white shadow-lg mt-6 p-6 rounded-lg">
+                        <h2 className="mb-4 font-semibold text-2xl text-gray-800">Transaction Trends</h2>
+                        <Line data={chartData!} />
+                    </div>
+                </>
+            ) : (
+                <div className="flex justify-center items-center h-96">
+                    <h2 className="text-2xl text-gray-500">No data to display</h2>
+                </div >
+            )}
+        </div >
+    );
+};
+
+interface DashBoardSettingsProps {
+    dateRange: DateRange;
+    setGranularity: (value: Granularity) => void;
+    granularity: Granularity;
+    updateDaterange: (range: DateRange) => void;
+    setDateRange: (range: DateRange) => void;
+    updateAnalyticsData: () => void;
+}
+
+function DashBoardSettings({ dateRange, setGranularity, granularity, setDateRange, updateAnalyticsData, updateDaterange }: DashBoardSettingsProps) {
+    return <div>
+        <div className="flex items-center mb-6">
+            <TbSettings className="mr-2 text-2xl" />
+            <h2 className="mb-2 font-semibold text-2xl text-gray-800">Settings </h2>
+        </div>
+        <div className="flex flex-wrap gap-4 mb-6">
+            {/* <div className="flex items-center space-x-2">
                         <Checkbox checked={groupByCurrency} onCheckedChange={(e) => setGroupByCurrency(e == true)} />
                         <span>Group by Currency</span>
                     </div> */}
-                    <DatePickerWithRange date={dateRange} setRange={updateDaterange} />
-                    <div className="flex items-center space-x-2">
-                        <Select onValueChange={(value) => setGranularity(value as Granularity)} >
-                            <SelectTrigger className="w-48" >{granularity ?? "Granularity"}</SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="day">Day</SelectItem>
-                                <SelectItem value="month">Month</SelectItem>
-                                <SelectItem value="year">Year</SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </div>
-
-                    <Button onClick={updateAnalyticsData} className="bg-indigo-600 hover:bg-indigo-700 text-white">Apply</Button>
-                    {/* clear button */}
-                    <Button onClick={() => setDateRange(undefined)} className="">Clear</Button>
-
-                </div>
+            <DatePickerWithRange date={dateRange} setRange={updateDaterange} />
+            <div className="flex items-center space-x-2">
+                <Select onValueChange={(value) => setGranularity(value as Granularity)} >
+                    <SelectTrigger className="w-48" >{granularity ?? "Granularity"}</SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="day">Day</SelectItem>
+                        <SelectItem value="month">Month</SelectItem>
+                        <SelectItem value="year">Year</SelectItem>
+                    </SelectContent>
+                </Select>
             </div>
 
-            {/* Stats Cards */}
-            <div className="gap-6 grid grid-cols-1 md:grid-cols-3">
-                <Card className="bg-white shadow-md">
-                    <CardHeader>
-                        <h2 className="font-semibold text-gray-800 text-xl">Granularity</h2>
-                    </CardHeader>
-                    <CardContent>
-                        <p className="font-semibold text-green-600 text-lg"> {granularity.toUpperCase()}</p>
-                    </CardContent>
-                </Card>
+            <Button onClick={updateAnalyticsData} className="bg-indigo-600 hover:bg-indigo-700 text-white">Apply</Button>
+            {/* clear button */}
+            <Button onClick={() => setDateRange(undefined)} className="">Clear</Button>
 
-                <Card className="bg-white shadow-md">
-                    <CardHeader>
-                        <h2 className="font-semibold text-gray-800 text-xl">Total INR Amount</h2>
-                    </CardHeader>
-                    <CardContent>
-                        <p className="font-semibold text-blue-600 text-lg">{analyticsData.reduce((acc, cur) => acc + parseFloat(cur.total_inr_amount), 0).toFixed(2)}</p>
-                    </CardContent>
-                </Card>
-
-                <Card className="bg-white shadow-md">
-                    <CardHeader>
-                        <h2 className="font-semibold text-gray-800 text-xl">Transaction Count</h2>
-                    </CardHeader>
-                    <CardContent>
-                        <p className="font-semibold text-lg text-yellow-600">{analyticsData.length}</p>
-                    </CardContent>
-                </Card>
-            </div>
-
-            {/* Chart Section */}
-            <div className="bg-white shadow-lg mt-6 p-6 rounded-lg">
-                <h2 className="mb-4 font-semibold text-2xl text-gray-800">Transaction Trends</h2>
-                <Line data={chartData!} />
-            </div>
         </div>
-    );
-};
+    </div>
+
+}
+
+interface ChartDashboardViewProps {
+    analyticsData: AnalyticsData[];
+    granularity: Granularity;
+}
+
+function ChartDashboardQuickInsights({ analyticsData, granularity }: ChartDashboardViewProps) {
+    return <>
+        <div className="gap-6 grid grid-cols-1 md:grid-cols-3">
+            <Card className="bg-white shadow-md">
+                <CardHeader>
+                    <h2 className="font-semibold text-gray-800 text-xl">Granularity</h2>
+                </CardHeader>
+                <CardContent>
+                    <p className="font-semibold text-green-600 text-lg"> {granularity.toUpperCase()}</p>
+                </CardContent>
+            </Card>
+
+            <Card className="bg-white shadow-md">
+                <CardHeader>
+                    <h2 className="font-semibold text-gray-800 text-xl">Total INR Amount</h2>
+                </CardHeader>
+                <CardContent>
+                    <p className="font-semibold text-blue-600 text-lg">{analyticsData.reduce((acc, cur) => acc + parseFloat(cur.total_inr_amount), 0).toFixed(2)}</p>
+                </CardContent>
+            </Card>
+
+            <Card className="bg-white shadow-md">
+                <CardHeader>
+                    <h2 className="font-semibold text-gray-800 text-xl">Transaction Count</h2>
+                </CardHeader>
+                <CardContent>
+                    <p className="font-semibold text-lg text-yellow-600">{analyticsData.length}</p>
+                </CardContent>
+            </Card>
+        </div>
+    </>
+
+
+}
 
 export default Dashboard;
